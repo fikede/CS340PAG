61cfb35478478b433948c0fb9739015bc15af54e
Jerry Farmer <jfarmer455@gmail.com>
2018-03-27 15:36:59 -0500

    I finished the matrix multiplication. I also added a private method getIndexOf( ), get and set methods, and an overriden toString( ).

--- 340GUI/PAG340MiddleWare/Matrix.cs
+++ 340GUI/PAG340MiddleWare/Matrix.cs
@@ -33,11 +33,103 @@ namespace PAG340MiddleWare
             return matrixTransposed;
         }
 
+        /*
+         * This is so that two matricies can be multiplied together.
+         * What it does is takes a row from the lhsMatrix and column from the rhsMatrix and preforms a dot product of the two.
+         * There is an exception thrown if a user tries to multiply two incompatible matrices.
+         * The product of the two matrices will also have the number of rows of the lhsMatrix and the number of columns of the rhsMatrix.
+         **/
         public static Matrix operator*(Matrix lhsMatrix, Matrix rhsMatrix)
         {
-            Matrix productMatrix = new Matrix(lhsMatrix.numberOfColumns, rhsMatrix.numberOfRows);
+            Matrix productMatrix;
 
+            if (lhsMatrix.numberOfColumns != rhsMatrix.numberOfRows)
+            {
+                Exception exception = new Exception("The number of columns of the left matrix must equal the number rows of the right matrix.");
+                throw exception;
+            }
+            else
+            {
+                int lhsNumberOfRows = lhsMatrix.numberOfRows;
+                int rhsNumberOfColumns = rhsMatrix.numberOfColumns;
+                int lhsNumberOfColumns = lhsMatrix.numberOfColumns;
+                //The resulting Matrix will have the same number of rows as the lhsMatrix and the same number of columns as the rhsMatrix
+                productMatrix = new Matrix(lhsNumberOfRows, rhsNumberOfColumns);
+                for(int index1 = 0; index1 < lhsNumberOfRows; index1++)
+                {
+                    for (int index2 = 0; index2 < rhsNumberOfColumns; index2++)
+                    {
+                        double result = 0.0;
+                        double lhsNumber;
+                        double rhsNumber;
+
+                        for(int index3 = 0; index3 < lhsNumberOfColumns; index3++)
+                        {
+                            lhsNumber = lhsMatrix.getNumberAt(index1, index3);
+                            rhsNumber = rhsMatrix.getNumberAt(index3, index2);
+                            result += (lhsNumber * rhsNumber);
+                        }
+
+                        productMatrix.setNumberAt(index1, index2, result);
+                    }
+                }
+
+            }
+            
             return productMatrix;
+        }
+
+        /*
+         * Every index in the array of a row and column number is found by
+         * index = columnNumber + rowNumber(numberOfColumns)
+         **/
+        private int getIndexOf(int rowNumber, int columnNumber)
+        {
+            int index = numberOfColumns;
+            index *= rowNumber;
+            index += columnNumber;
+            return index;
+        }
+
+        public int NumberOfRows
+        {
+            get { return numberOfRows; }
+            set { numberOfRows = value; }
+        }
+
+        public int NumberOfColumns
+        {
+            get { return numberOfColumns; }
+            set { numberOfColumns = value; }
+        }
+
+        public double getNumberAt(int rowNumber, int columnNumber)
+        {
+            int index = getIndexOf(rowNumber, columnNumber);
+            double numberAt = matrix[index];
+            return numberAt;
+        }
+
+        public void setNumberAt(int rowNumber, int columnNumber, double numberToBeSet)
+        {
+            int index = getIndexOf(rowNumber, columnNumber);
+            matrix[index] = numberToBeSet;
+        }
+
+
+        public override string ToString()
+        {
+            string output = "Number of Rows: " + numberOfRows + ", Number of Columns: " + numberOfColumns + " ";
+            for(int index1 = 0; index1 < numberOfRows; index1++)
+            {
+                output += "\n";
+                for(int index2 = 0; index2 < numberOfColumns; index2++)
+                {
+                    double numberAt = getNumberAt(index1, index2);
+                    output += numberAt + "   ";
+                }
+            }
+            return output;
         }
     }
 }
